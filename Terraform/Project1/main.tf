95664a55fae72cb8c96e4dec4b2684ff860fb819
SivaKumar <sivakumar.k@comakeit.com>
2022-03-04 14:24:10 +0530

    Changes AWS Creation File
    Added Security Groups
    Added KeyPair association

--- Terraform/Project1/main.tf
+++ Terraform/Project1/main.tf
@@ -1,16 +1,36 @@
- # Deploying an EC2 server using terraform
+# Deploying an EC2 server using terraform
 
 provider "aws" {
-  region = "ap-south-1"
-    access_key = var.access_key
-    secret_key = var.secret_key
+  region     = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 resource "aws_instance" "practise-server" {
-    ami =  "ami-0851b76e8b1bce90b"
-    instance_type = "t2.micro"
+  ami             = "ami-0851b76e8b1bce90b"
+  instance_type   = "t2.micro"
+  key_name        = "Red_hat"
+  vpc_security_group_ids = ["${aws_security_group.practise-server-sg.id}"]
+  tags = {
+    Name = "CubeWorks_Nginx_POC"
+  }
 
-    tags = {
-        Name = "Terraform_server"
-    }
+}
+
+resource "aws_security_group" "practise-server-sg" {
+  name        = "Project_sec_grp"
+  description = "Security_Group"
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["103.138.44.194/32"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
